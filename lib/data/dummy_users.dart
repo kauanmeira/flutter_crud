dadace169071f700cf3f637c3d7482a2cd5e80cf
kauan.meira <kauangabriel.meira123@gmail.com>
2023-07-19 09:32:54 -0300

    Primeiro commit, criação de listas de usuários, com avatares

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/User.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Kauan',
+      email: 'kauan@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png'),
+  '2': const User(
+      id: '2',
+      name: 'Stephanie',
+      email: 'stephanie@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/03/01/22/18/avatar-2109804_1280.png'),
+  '3': const User(
+      id: '3',
+      name: 'Teste',
+      email: 'teste@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/12/19/15/gangster-154425_1280.png'),
+  '4': const User(
+      id: '4',
+      name: 'Teste2',
+      email: 'teste2@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/09/30/08/53/woman-6669461_1280.png')
+};
